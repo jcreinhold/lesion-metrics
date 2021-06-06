337a130e13a5d075be6b72aa717a234adcbadcfe
Jacob Reinhold <jcreinhold@gmail.com>
2021-06-06 11:53:09 -0400

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -56,6 +56,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/jcreinhold/lesion-metrics",
-    version="0.1.2",
+    version="0.1.3",
     zip_safe=False,
 )
