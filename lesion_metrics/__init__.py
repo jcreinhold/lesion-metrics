9e65d0090783cef0877bb2333abf323f8927141d
Jacob Reinhold <5241441+jcreinhold@users.noreply.github.com>
2022-01-21 15:03:25 -0500

    Bump version: 0.1.11 → 0.1.12

--- lesion_metrics/__init__.py
+++ lesion_metrics/__init__.py
@@ -5,7 +5,7 @@ __description__ = "Metrics for evaluating lesion segmentations"
 __url__ = "https://github.com/jcreinhold/lesion-metrics"
 __author__ = """Jacob Reinhold"""
 __email__ = "jcreinhold@gmail.com"
-__version__ = "0.1.11"
+__version__ = "0.1.12"
 __license__ = "Apache-2.0"
 __copyright__ = "Copyright 2021 Jacob Reinhold"
 
